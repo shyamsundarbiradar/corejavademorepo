762942072b73b72899a521259378e0f2f3e11e3f
shyambiradar <biradarshyam1999@gmail.com>
2022-05-23 05:07:34 +0000

    first project

---
+++ Biography.java
@@ -0,0 +1,26 @@
+package InitialName;
+
+public class Biography {
+	public static void main(String[] args) {
+	String name="shyam";
+    int age=23;
+    String country="india";
+    String sport="cricket";
+    float hours =2.5f;
+    String game= "PUBG";
+    String subject= " history";
+    char grade= 'B';
+    System.out.print(">> My Name is "+name+ ".");
+    System.out.print(" I'm "+ age +" years old,");
+    System.out.println(" and I'm from "+country+ ".");
+    System.out.print("My favorite sport is "+sport);
+    System.out.print(", I play for "+hours+ "hour(s) a day");
+    
+    
+    
+    
+    
+    
+    
+}
+}
